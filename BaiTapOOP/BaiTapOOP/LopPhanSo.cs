1b673c79806b8bcceebc7dc0220d909440a61a90
tonypham90 <tuananh.gt@hotmail.com>
2022-07-10 20:59:27 +0700

    asdf
    asdf

---
+++ BaiTapOOP/BaiTapOOP/LopPhanSo.cs
@@ -0,0 +1,29 @@
+namespace BaiTapOOP;
+
+public class LopPhanSo
+{
+    public int Tuso,Mauso;
+
+    public void InputValue(string ghichu)
+    {
+        Console.WriteLine(ghichu);
+        Console.Write("Tuso: ");
+        this.Tuso = int.Parse(Console.ReadLine());
+        Console.Write("MauSo:");
+        this.Mauso = int.Parse(Console.ReadLine());
+    }
+
+    public LopPhanSo Sum(LopPhanSo phanSo)
+    {
+        Console.WriteLine($"Tong phan so {this.Tuso}/{this.Mauso} va {phanSo.Tuso}/{phanSo.Mauso}");
+        LopPhanSo result = new LopPhanSo();
+        result.Tuso = phanSo.Tuso * this.Mauso + phanSo.Mauso * this.Tuso;
+        result.Mauso = phanSo.Mauso * this.Mauso;
+        return result;
+    }
+
+    public string PrintPhanSo()
+    {
+        
+    }
+}
